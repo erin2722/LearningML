e6d15e4a5368f02f94a1f40fad8bfd98eb43a962
erin2 <erin2722@gmail.com>
2019-12-23 17:17:09 -0500

    add files

---
+++ myFirstMachineLearning.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+
+data = np.random.random((1000, 32))
+labels = np.random.random((1000, 10))
+
+model = tf.keras.Sequential()
+# Adds a densely-connected layer with 64 units to the model:
+model.add(layers.Dense(64, activation='relu'))
+# Add another:
+model.add(layers.Dense(64, activation='relu'))
+# Add a softmax layer with 10 output units:
+model.add(layers.Dense(10, activation='softmax'))
+
+# Create a sigmoid layer:
+layers.Dense(64, activation='sigmoid')
+# Or:
+layers.Dense(64, activation=tf.sigmoid)
+
+# A linear layer with L1 regularization of factor 0.01 applied to the kernel matrix:
+layers.Dense(64, kernel_regularizer=tf.keras.regularizers.l1(0.01))
+
+# A linear layer with L2 regularization of factor 0.01 applied to the bias vector:
+layers.Dense(64, bias_regularizer=tf.keras.regularizers.l2(0.01))
+
+# A linear layer with a kernel initialized to a random orthogonal matrix:
+layers.Dense(64, kernel_initializer='orthogonal')
+
+# A linear layer with a bias vector initialized to 2.0s:
+layers.Dense(64, bias_initializer=tf.keras.initializers.constant(2.0))
+
+model = tf.keras.Sequential([
+# Adds a densely-connected layer with 64 units to the model:
+layers.Dense(64, activation='relu'),
+# Add another:
+layers.Dense(64, activation='relu'),
+# Add a softmax layer with 10 output units:
+layers.Dense(10, activation='softmax')])
+
+model.compile(optimizer=tf.train.AdamOptimizer(0.001),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(data, labels, epochs=10, batch_size=32)
